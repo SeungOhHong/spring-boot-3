5842e3c58f5e75e3c3afff9aef4dddc9ade45ba1
SeungOhHong <ghdtmddh@gmail.com>
2023-03-31 00:23:00 +0900

    컴포넌트 스캐닝 메인패키지와 서브패키지

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -1,10 +1,15 @@
 package com.luv2code.springcoredemo;
 
+// 스프링 부트는 com.luv2code.springcoredemo 패키지 안에 있는 서브패키지만 스캐닝한다
+// 그 밖에 있는 패키지에 대한 스캐닝을 하고 싶으면 SpringcoredemoApplication 에서 따로 설정해줘야한다
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
 @SpringBootApplication(
+	
+	이런 식으로 com.luv2code.util까지 컴포넌트 스캐닝의 대상이라고 base 패키지를 명시해줘야한다
 		scanBasePackages = {"com.luv2code.springcoredemo",
 							"com.luv2code.util"})
 */
--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -9,3 +9,12 @@ public class CricketCoach implements Coach {
         return "Practice fast bowling for 15 minutes";
     }
 }
+
+// # 컴포넌트 스캐닝
+// 스프링은 특수한 어노테이션이 붙은 자바 클래스들을 스캔한다 ex) @Component 어노테이션
+// 그리고 스프링 컨테이너에 빈들을 자동 등록한다
+
+// rest와 common 두가지의 패키지를 생성한다
+// 이제 이 두 패키지는 com.luv2code.springcoredemo 라는 메인 패키지의 서브 패키지가 된다
+// Coach와 CricketCoach는 common 패키지에 / DemoController는 rest 패키지에 옮긴다
+
