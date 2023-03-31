ee44898d8e194f74b7c9e75af92fbf6b8f84774e
SeungOhHong <ghdtmddh@gmail.com>
2023-03-31 15:16:17 +0900

    빈 라이프사이클 메서드 @PostConstruct & @PreDestroy

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -11,3 +11,13 @@ public class SpringcoredemoApplication {
 	}
 
 }
+
+/* 	빈 라이프사이클
+* 컨테이너 시작 -> 빈 초기화 -> 의존성 주입 -> 스프링 내부 프로세싱 -> 커스텀 메서드
+* 
+* 빈 초기화와 빈 파괴 동안 커스텀 코드를 추가할 수 있다
+* @PostConstruct 
+* @PreDestroy
+*	에노테이션을 이용한다
+*
+* */
--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -11,13 +11,14 @@ public class CricketCoach implements Coach {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
 
-    // define our init method
+    // 초기화 메서드 정의
     @PostConstruct
     public void doMyStartupStuff() {
+
         System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
     }
 
-    // define our destroy method
+    // 파괴 메서드 정의
     @PreDestroy
     public void doMyCleanupStuff() {
         System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
@@ -29,3 +30,18 @@ public class CricketCoach implements Coach {
         return "Practice fast bowling for 15 minutes";
     }
 }
+
+/* 빈 라이프사이클 메서드
+*   단계 1: 초기화 혹은 파괴를 위한 메서드 정의
+*   단계 2: @PostConstruct & @PreDestroy 에노테이션 덧붙이기
+* 
+* 앱을 구동하면 초기화 메서드가 실행된다
+*   In constructor: BaseballCoach
+In constructor: CricketCoach
+In doMyStartupStuff(): CricketCoach  -> PostConstruct 되었다
+In constructor: TennisCoach
+In constructor: TrackCoach
+In constructor: DemoController
+* 
+* 앱을 종료하면 파괴 메서드가 실행된다
+* */
