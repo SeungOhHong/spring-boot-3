904dfb76dcad3d2b928242f18c32c58279a01e19
SeungOhHong <ghdtmddh@gmail.com>
2023-03-31 11:50:27 +0900

    스프링 컨테이너 의존성 주입 방법2 : 새터인젝션 &필드인젝션(레거시)

--- 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -9,3 +9,24 @@ public class CricketCoach implements Coach {
         return "Practice fast bowling for 15 minutes";
     }
 }
+
+// 종속성 주입 방법2 - 새터 인젝션
+/*
+* 1. 인젝션을 위해서 클래스에 새터 메서드를 생성한다
+* ex) setCoach 메서드를 생성한다
+* 2. @Autowired 어노테이션을 이용하여 DI를 설정한다
+* setCoach 메서드에 @Autowired 어노테이션을 이용한다
+*
+* DI는 스프링 프레임워크 내부적으로 이루어진다
+* Coach 인터페이스 - CricketCoach(Coach의 구현체) - DemoController 
+*
+* 
+* */
+
+// 종속성 주입 방법3 - 필드 인젝션(레거시)
+/*
+*  필드 인젝션으로 DI를 할 경우에는 유닛 테스트가 힘들어져서 잘 쓰지 않는다
+*  private 필드에 직접적으로 @AutoWired 어노테이션을 붙인다. 별도의 새터나 생성자는 필요없지만
+*  유닛 테스트가 힘들어져서 레거시가 되었다
+*
+* */
--- 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-
+    
+    // 이전 생성자 주입 코드를 지워준다
+    
     // define a private field for the dependency
     private Coach myCoach;
 
+    // 새터 메서드를 어노테이션과 함께 생성해준다
+    // @Autowired 어노테이션이 붙어있기 때문에 메서드의 이름을 바꿔도 DI의 대상으로 인식된다
+    // ex) setCoach -> doSomeStuff
     @Autowired
     public void setCoach(Coach theCoach) {
         myCoach = theCoach;
